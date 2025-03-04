94db7ce25238626edf9f29b4d88213d8c71e34cf
rina <k@rina.fyi>
2025-03-04 15:01:41 +1000

    test base

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
+def create_app(config_overrides=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+    if config_overrides:
+        app.config.update(config_overrides)
     # Load the models.
     from todo.models import db
     from todo.models.todo import Todo
@@ -13,6 +15,6 @@ def create_app():
         db.create_all()
         db.session.commit()
     # Register the blueprints.
-    from todo.views.routes import api
+    from .views.routes import api
     app.register_blueprint(api)
     return app
